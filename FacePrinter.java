da86922d13076ed06d675078c67b646962a30d1b
Beltran <mbeltran11@I46221P64128186.local>
2019-06-12 12:06:51 -0700

    adding faceprinter

---
+++ FacePrinter.java
@@ -0,0 +1,17 @@
+public class FacePrinter
+{
+public static void main(String[] args)
+{
+    System.out.println("  ___________  ");
+    System.out.println( "|           |");
+    System.out.println("|             |");
+    System.out.println("|   --   --   |");
+    System.out.println("|      |      |");
+    System.out.println("|             |");
+    System.out.println("|   -------   |");
+    System.out.println("    ________   ");
+
+
+
+}
+}
